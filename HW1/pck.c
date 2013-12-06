558e06374c6b016b236a6bb09561598d4e5a5aaf
choupi <chromosome460@gmail.com>
2013-12-06 18:05:22 +0800

    Verify with test file

--- HW1/pck.c
+++ HW1/pck.c
@@ -11,18 +11,21 @@ void shuf(int *nn, int n) {
     }
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+    //FILE* train=fopen(argv[1], "r");
+    FILE* test=fopen(argv[1], "r");
     int i, j, k, n, p, flag;
     float x[1000][4];
     int y[1000];
     float w[5]={0.0};
     float s;
+    float x1, x2, x3, x4;
     int nn[1000];
     srand(time(NULL));
 
     i=0;
-    while(EOF!=scanf(" %f %f %f %f %d", x[i], x[i]+1, x[i]+2, x[i]+3, y+i)) {
+    while(EOF!=scanf(" %f %f %f %f %d", x[i],x[i]+1,x[i]+2,x[i]+3,y+i)) {
         //printf("%f %f %f %f %d\n", x[i][0], x[i][1], x[i][2], x[i][3], y[i]);
         i++;
     }
@@ -31,7 +34,7 @@ int main()
     shuf(nn, n);
     k=0;
     flag=1;
-    while(flag) {
+    while(flag && k<50) {
       flag=0;
       shuf(nn, n);
       for(i=0;i<n;i++) {
@@ -44,8 +47,16 @@ int main()
 	k++;
 	for(j=0;j<4;j++) w[j]+=y[p]*x[p][j];
 	w[4]+=y[p];
+	if(k>=50) break;
       }
     }
-    for(j=0;j<5;j++) printf("%f ", w[j]);
-    printf("\nupdate=%d\n",k);
+    //for(j=0;j<5;j++) printf("%f ", w[j]);
+    //printf("\nupdate=%d\n",k);
+    n=0; k=0;
+    while(EOF!=fscanf(test, " %f %f %f %f %d", &x1, &x2, &x3, &x4, &p)) {
+        n++;
+	s=x1*w[0]+x2*w[1]+x3*w[2]+x4*w[3]+w[4];
+	if(s*p>0) k++;
+    }
+    printf("%f\n", (float)k/n);
 }
