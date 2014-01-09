c3475ba3aa1cadca4258a061b656048db3b22248
choupi <chromosome460@gmail.com>
2014-01-09 20:24:11 +0800

    fixed rate gradient descent algorithm for logistic regression

--- HW3/logReg.c
+++ HW3/logReg.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <math.h>
 
+#define MAXN (3000)
+#define ITER (2000)
 #define theta(x) (1.0/(1+exp(-1*x)))
 
 void printVector(char vn, float* V, int n)
@@ -48,16 +50,45 @@ void logRegIter(float* X, int* Y, float* w, int N, int d, float anta)
     sAxpy(s, w, t, d);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    float X[6]={1,2,3,4,5,6};
-    int Y[3]={1,1,-1};
-    float w[2]={0,0};
-    int i;
-    for(i=0;i<10;i++) {
-	logRegIter(X, Y, w, 3, 2, 0.01);
-	printVector('w', w, 2);
+    FILE* test=fopen(argv[1], "r");
+    int i=0, j, k=0, d=20, N;
+    float* X=(float*)malloc(MAXN*d*sizeof(float));
+    int* Y=(int*)malloc(MAXN*sizeof(int));
+    float* w=(float*)malloc(d*sizeof(float));
+    printf("%s", argv[1]);
+
+    while(1) {
+        for(j=0;j<d;j++) {
+            if(EOF==scanf(" %f", X+i*d+j)) { k=1; break; }
+        }
+        if(EOF==scanf(" %d", Y+i)) { k=1; break; }
+        if(k) break;
+        i++;
     }
-    for(i=0;i<3;i++) printf("%f ", sDot(X+i*2, w, 2));
+    N=i;
+
+    for(i=0;i<d;i++) w[i]=0.0;
+    for(i=0;i<ITER;i++) {
+	logRegIter(X, Y, w, N, d, 0.001);
+	//printVector('w', w, d);
+    }
+    //for(i=0;i<N;i++) printf("%f ", sDot(X+i*d, w, d));
+    //printf("\n");
+    printVector('w', w, d);
+    i=0; k=0;
+    while(1) {
+        for(j=0;j<d;j++) {
+            if(EOF==fscanf(test, " %f", X+i*d+j)) { k=1; break; }
+        }
+        if(EOF==fscanf(test, " %d", Y+i)) { k=1; break; }
+        if(k) break;
+        i++;
+    }
+    N=i;
+    k=0;
+    for(i=0;i<N;i++) if(sDot(X+i*d, w, d)*Y[i]>0) k++;
+    printf("%d %d %f\n", k, N, 1-(float)k/N);
 }
 
